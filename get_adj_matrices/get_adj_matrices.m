9c2018b67ce9b6aab77591c86b37057c05fa6967
Erin <erincconrad@gmail.com>
2019-04-24 18:29:01 -0400

    made script for getting adjacency matrices

--- get_adj_matrices/get_adj_matrices.m
+++ get_adj_matrices/get_adj_matrices.m
@@ -19,16 +19,15 @@ for i = 1:nchs
         % that was taking up the majority of the computational time. I
         % expect that I cannot call this with additional arguments because
         % of this change.
-        cxy = mscohere_erin(x,y);
-        f = 0:2:fs/2;
+        [cxy,w] = mscohere_erin(x,y);
+        f = w*fs*2/pi;
         
-      %  [cxy_old,f] = mscohere(x,y,[],[],[],fs);
+       % [cxy_old,f_old] = mscohere(x,y,[],[],[],fs);
         
         for ff = 1:size(freq_bands,1)
             adj(ff).adj(i,j) = mean(cxy(f>=freq_bands(ff,1)&...
                 f<=freq_bands(ff,2)));
-            adj(ff).adj(j,i) = mean(cxy(f>=freq_bands(ff,1)&...
-                f<=freq_bands(ff,2)));
+            adj(ff).adj(j,i) = adj(ff).adj(i,j);
             
         end
         %}
--- get_adj_matrices/spike_networks.m
+++ get_adj_matrices/spike_networks.m
@@ -1,13 +1,14 @@
 function spike_networks(whichPts)
 
 %% Parameters
+merge = 1; % merge with existing?
 do_car = 1;
-pre_whiten = 0;
+pre_whiten = 1;
 time_window = 1; %1 second time window
-n_chunks = 10;
+n_chunks = 11;
 
 % The time surrounding the spike peak that I will call the spike window
-spike_window = [-0.1 0.9];
+spike_window_times = [-0.2 0.8];
 freq_bands = [5 15;... %alpha/theta
     15 25;... %beta
     30 40;... % low gamma
@@ -29,8 +30,8 @@ addpath(genpath(script_folder));
 spike_times_file = [data_folder,'spike_times/times.mat'];
 pt_file = [data_folder,'spike_structures/pt.mat'];
 
-times = load(spike_times_file); % will result in a structure called "times"
-times = times.times;
+times = load(spike_times_file); % will result in a structure called "out"
+times = times.out;
 pt = load(pt_file); % will create a structure called "pt"
 pt = pt.pt;
 
@@ -48,10 +49,17 @@ for whichPt = whichPts
     name = times(whichPt).name;
     fprintf('\nDoing %s\n',name);
     
+    
     % Get basic data about patient
     pt_folder = [results_folder,name,'/'];
     data = load([pt_folder,'basic_info.mat']); % returns a structure called data
     data = data.data;
+    
+     % output folder
+    out_folder = [pt_folder,'adj/'];
+    if exist(out_folder,'dir') == 0
+        mkdir(out_folder);
+    end
     
     % convert ch labels to nice form and decide which to ignore
     ch_labels = data.chLabels(:,1);
@@ -68,12 +76,40 @@ for whichPt = whichPts
     
     % Loop through spike files
     for f = 1:10
+        
+        % Skip it if already done
+        if merge == 1
+            if exist([out_folder,sprintf('finished_%d.mat',f)],'file') ~=0
+                fprintf('Did file %d already, skipping...\n',f);
+                continue
+            end
+            
+        end
+        
         fprintf('Doing file %d of %d...\n',f,10);
         spike = load([pt_folder,sprintf('spikes_%d.mat',f)]);
         spike = spike.spike;
         
+        % Initialize output data
+        meta_file = [out_folder,sprintf('adj_%d.mat',f)];
+        
+        % Load it if it exists to see how much we've already done
+        if exist(meta_file,'file') ~= 0
+            meta = load(meta_file);
+            meta = meta.meta;
+            
+            % Find first unfinished spike
+            start_spike = length(meta.spike) + 1;
+        else
+            start_spike = 1;
+            meta.name = name;
+            meta.which_file = f;
+            meta.file_name = sprintf('spikes_%d.mat',f);
+        end
+        
+        
         % Loop through spikes
-        for s = 1:length(spike)
+        for s = start_spike:length(spike)
             fprintf('Doing spike %d of %d...\n',s,length(spike));
             tic
             if isempty(spike(s).time) == 1, continue; end
@@ -83,35 +119,61 @@ for whichPt = whichPts
             % Grab the appropriate channels
             values = spike(s).values(:,~ignore);
             
-            % test plot of spike
-            if 0
-                
-                % Get spike ch
-                is_sp_ch = strcmp(ch_labels,spike(s).label);
-                sp_data = spike(s).values(:,is_sp_ch);
-                is_seq_ch = ismember(ch_labels,spike(s).seq_labels);
-                figure
-                plot(sp_data);
-                pause
-                close(gcf)
-            end
+            % Get spike chs
+            is_sp_ch = strcmp(ch_labels(~ignore),spike(s).label);
+            is_seq_ch = ismember(ch_labels(~ignore),spike(s).seq_labels);
+           
             
-            %% Confirm spike peak time
-            
-            
+            meta.spike(s).time =spike(s).time;
+            meta.spike(s).is_sp_ch = is_sp_ch;
+            meta.spike(s).is_seq_ch = is_seq_ch;
+ 
             %% Pre-processing
             % Parameters 2 and 3 indicate whether to do CAR and pre-whiten,
             % respectively
             %fprintf('Doing pre-processing...\n');
+            old_values = values;
             values = pre_processing(values,do_car,pre_whiten);
             nchs = size(values,2);
             
+            %% Figure out times for which I will be calculating adjacencies
+            % The peak should be the very center of each file
+            peak = round(size(values,1)/2);
+            
+            % Get index windows
+            index_windows = zeros(n_chunks,2); %6 is the spike window
+            tick_window = time_window*data.fs;
+            
+            % Get the time that I will call my spike window (that I will
+            % remove when doing analysis)
+            spike_window = peak + spike_window_times*data.fs;
+            
+            % Show the spike window
+            if 0
+                sp_data = old_values(:,is_sp_ch);
+                figure
+                plot(sp_data);
+                hold on
+                plot(values(:,is_sp_ch))
+                plot([spike_window(1) spike_window(1)],get(gca,'ylim'),'k--')
+                plot([spike_window(2) spike_window(2)],get(gca,'ylim'),'k--')
+                plot([peak peak],get(gca,'ylim'),'b--');
+                beep
+                pause
+                close(gcf)
+            end
+            
+            for i = 1:size(index_windows,1)
+                
+                % if we're at the spike window (6), then the index window
+                % is the spike window. If i is 1, then it's 5 seconds
+                % before. If i is 11, it's 5 seconds after
+                index_windows(i,:) = spike_window + tick_window*(i-6);
+            end
+            
             %% Get adjacency matrices
             %fprintf('Calculating functional networks...\n');
-            % Break the data up into time chunks
-            tick_window = time_window*data.fs;
-            n_chunks = floor(size(values,1)/tick_window);
-            
+
             % Initialize adjacency matrix
             for ff = 1:size(freq_bands,1)
                 adj(ff).name = freq_names{ff};
@@ -121,8 +183,7 @@ for whichPt = whichPts
             for tt = 1:n_chunks
                 
                 % get appropriate points
-                temp_values = values((tt-1)*tick_window+1:...
-                    min(tt*tick_window,size(values,1)),:); % this will cut off last point for some, which is fine
+                temp_values = values(round(index_windows(tt,1)):round(index_windows(tt,2)),:); 
                 
                 % Get adjacency matrices
                 t_adj = get_adj_matrices(temp_values,data.fs,freq_bands);
@@ -132,23 +193,34 @@ for whichPt = whichPts
                 end
                 
             end
+            
+            meta.spike(s).adj = adj;
+            
+            % Save the meta file after each spike run
+            save(meta_file,'meta');
 
             t = toc;
             fprintf('Spike %d took %1.1f minutes.\n\n',s,t/60);
             
-            if 1
+            if 0
                 figure
                 set(gcf,'position',[200 250 1175 400]);
-                for tt = 1:n_chunks
+                for tt = 1:n_chunks-1
                     subplot(2,5,tt);
                     imagesc(squeeze(adj(4).adj(tt,:,:)));
                     colorbar
                     title(sprintf('%d s',tt))
                 end
+                beep
                 pause
                 close(gcf)
             end
         end
+        
+        % Once I've done all spikes for the file, make a finished.mat
+        finished = 1;
+        save([out_folder,sprintf('finished_%d.mat',f)],'finished');
+        
     end
     
 end
