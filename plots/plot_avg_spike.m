54f7f375cc3c295ddacb4308b496ee8cf61f51cb
Erin <erincconrad@gmail.com>
2020-08-07 09:50:01 -0400

    changed sig dev code to include pre processing

--- plots/plot_avg_spike.m
+++ plots/plot_avg_spike.m
@@ -16,7 +16,7 @@ addpath(genpath(script_folder));
 pt_file = [data_folder,'spike_structures/pt.mat'];
 bct_folder = locations.BCT;
 addpath(genpath(bct_folder));
-out_folder = [results_folder,'plots/'];
+out_folder = [results_folder,'plots/avg_spikes/'];
 sig_dev_folder = [results_folder,'signal_deviation/manual/'];
 
 
@@ -51,32 +51,72 @@ for i = 1:length(listing)
     surround = spike(1).surround_time;
     fs = spike(1).fs;
     
+    ch_devs_all = zeros(nspikes,1);
+    
     for s = 1:length(spike)
         involved = spike(s).involved;
         
-        values = spike(s).data;
+        if sum(involved) == 0, continue; end
+        chs_involved = find(involved);
         
+        x = spike(s).data;
         % pre process
-        values = pre_processing(values,do_car,pre_whiten,do_notch,fs);
+        x = pre_processing(x,do_car,pre_whiten,do_notch,fs);
         
-        x = values(:,involved);
+        
+        x = x(:,involved);
+      
         
         % get baseline
         bl = median(x,1);
         
         % get dev
         dev = abs(x-bl);
-        
+                
         % avg across spike chs
         avg_dev = mean(dev,2);
         
         dev_all(:,s) = avg_dev;
         
+        % Get max dev for each ch
+        max_over_time = max(dev,[],1);
+        
+        % Find channel with biggest dev
+        [~,biggest_dev_ch] = max(max_over_time);
+        
+        ch_devs_all(s) = chs_involved(biggest_dev_ch);
+        
     end
     
     final_dev = mean(dev_all,2);
+    
+    % take mode channel with biggest dev
+    mode_ch = mode(ch_devs_all);
+    
+    mode_ch_dev = zeros(size(spike(1).data,1),nspikes);
+    % get avg dev for that ch
+    for s = 1:length(spike)
+        x = spike(s).data;
+        x = pre_processing(x,do_car,pre_whiten,do_notch,fs);
+        x = x(:,mode_ch);
+        
+        bl = median(x,1);
+        mode_ch_dev(:,s) = abs(x-bl);
+    end
+    mode_ch_dev = mean(mode_ch_dev,2);
+    
+    figure
+    set(gcf,'position',[440 58 932 740])
+    subplot(2,1,1)
     plot(linspace(-surround,surround,length(final_dev)),final_dev)
-    pause
+    title(sprintf('%s',pt_name))
+    set(gca,'fontsize',20)
+    subplot(2,1,2)
+    plot(linspace(-surround,surround,length(final_dev)),mode_ch_dev)
+    title(sprintf('Biggest dev ch: %d',mode_ch));
+    set(gca,'fontsize',20)
+    print([out_folder,pt_name],gcf,'-depsc');
+    close(gcf)
         
 end
 
