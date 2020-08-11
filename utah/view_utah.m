b953226ece532413aa7275d25128ce2e26a2902e
Erin <erincconrad@gmail.com>
2020-08-11 16:52:17 -0400

    added Columbia data script

--- utah/view_utah.m
+++ utah/view_utah.m
@@ -1,9 +1,10 @@
 function view_utah
 
-plot_time = [0 15]; % time to plot in seconds
+plot_time = 15; % time to plot in seconds
+goal_fs = 500;
 
 %% Parameters
-do_notch = 0; % notch filter?
+do_notch = 1; % notch filter?
 do_car = 1; % common average reference?
 pre_whiten = 0; % remove the AR(1) component for a pre-whitening step?
 
@@ -15,6 +16,9 @@ sleep_utah_folder = [grid_folder,'interictal_sleep_utah_0160_043647/'];
 
 listing = dir(sleep_utah_folder);
 
+figure
+set(gcf,'position',[75 53 1311 752]);
+
 for i = 1:length(listing)
     if contains(listing(i).name,'.mat') == 0, continue; end
     fname = listing(i).name;
@@ -22,35 +26,62 @@ for i = 1:length(listing)
     data = data.data;
     
     %% Grab basic info
-    fs = data.MetaTags.SamplingFreq;
+    old_fs = data.MetaTags.SamplingFreq;
     elecs = data.ElectrodesInfo;
     
     %% EEG data
     values = double(data.Data);
-    plot_idx = max([1,round(plot_time(1)*fs)]):...
-        min([size(values,2),round(plot_time(2)*fs)]);
-    nchs = size(values,1);
+    values = values';
+    total_time = size(values,1)/old_fs;
+    
+    %% Downsample to 500 Hz
+    n_downsample = round(old_fs/goal_fs);
+    values = downsample(values,n_downsample);
+    fs = size(values,1)/total_time;
+    
+    
+    nchs = size(values,2);
     
     %% Pre-processing
-    temp_values = pre_processing(values',do_car,pre_whiten,do_notch,fs);
-    values = temp_values'; % pre-processing assumes it's ntimepointsxnchs
+    values = pre_processing(values,do_car,pre_whiten,do_notch,fs);
+
     
-    figure
-    set(gcf,'position',[75 53 1311 752]);
-    offset = 0;
-    for ich = 1:nchs
-        plot(linspace(plot_time(1),plot_time(2),length(plot_idx)),...
-            values(ich,plot_idx)+offset);
-        hold on
-        if ich ~= nchs
-            offset = offset - abs((prctile(values(ich+1,plot_idx),95) - prctile(values(ich,plot_idx),95)));
-            %offset = offset - abs((max(values(ich+1,plot_idx)) - min(values(ich,plot_idx))));
+    
+    start_idx = 1;
+    
+    while 1
+        plot_idx = start_idx:...
+            min([size(values,1),round(plot_time*fs)+start_idx]);
+        
+
+        offset = 0;
+        for ich = 1:nchs
+            plot(linspace(0,plot_time,length(plot_idx)),...
+                values(plot_idx,ich)+offset);
+            hold on
+            if ich ~= nchs
+                offset = offset - abs((prctile(values(plot_idx,ich+1),98) - prctile(values(plot_idx,ich),2)));
+                %offset = offset - abs((max(values(ich+1,plot_idx)) - min(values(ich,plot_idx))));
+            end
+
+        end
+        
+        pause
+        
+        if size(values,1)>round(plot_time*fs)+start_idx
+            start_idx = round(plot_time*fs)+start_idx;
+            hold off
+        else
+            hold off
+            break
         end
     
     end
     
     
     
+end
+
 end
 
 end
