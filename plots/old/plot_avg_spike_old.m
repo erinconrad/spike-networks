2cc1a925ff1f8af688b4e53398f82995abf569bd
Erin <erincconrad@gmail.com>
2020-08-11 07:01:25 -0400

    subtract baseline before getting power

--- plots/old/plot_avg_spike_old.m
+++ plots/old/plot_avg_spike_old.m
@@ -73,69 +73,61 @@ for i = 1:length(listing)
     nspikes = length(spike);
     
     dev_all = zeros(size(spike(1).data,1),nspikes);
-    dev_all_involved = zeros(size(spike(1).data,1),nspikes);
     surround = spike(1).surround_time;
     fs = spike(1).fs;
     
     ch_devs_all = zeros(nspikes,1);
     
-    
     for s = 1:length(spike)
+        involved = spike(s).involved;
         
-        % biggest deviation channel
-        biggest_dev = spike(s).biggest_dev;
-        all_involved = spike(s).involved;
+        if sum(involved) == 0, continue; end
+        chs_involved = find(involved);
         
         x = spike(s).data;
-        
         % pre process
         x = pre_processing(x,do_car,pre_whiten,do_notch,fs);
         
-        % all involved
-        y = x(:,all_involved);
         
-        % just most involved ch
-        x = x(:,biggest_dev);
+        x = x(:,involved);
+      
         
-        
-              
         % get baseline
         bl = median(x,1);
-        bly = median(y,1);
         
         % get dev
-        dev = (abs(x-bl)).^2;
-        dev_involved = (abs(y-bly)).^2;
+        dev = abs(x-bl);
                 
+        % avg across spike chs
+        avg_dev = mean(dev,2);
         
-        if size(dev_all,1) > size(dev,1)
-            dev = [dev;...
-                repmat(dev(end),size(dev_all,1)-size(dev,1),1)];
-        elseif size(dev_all,1) < size(dev,1)
-            dev(end-(size(dev,1)-size(dev_all,1))+1:end) = [];
+        if size(dev_all,1) > size(avg_dev,1)
+            avg_dev = [avg_dev;...
+                repmat(avg_dev(end),size(dev_all,1)-size(avg_dev,1),1)];
+        elseif size(dev_all,1) < size(avg_dev,1)
+            avg_dev(end-(size(avg_dev,1)-size(dev_all,1))+1:end) = [];
         end
         
-        if size(dev_all_involved,1) > size(dev_involved,1)
-            dev_involved = [dev_involved;...
-                repmat(dev_involved(end),size(dev_all_involved,1)-size(dev_involved,1),1)];
-        elseif size(dev_all_involved,1) < size(dev_involved,1)
-            dev_involved(end-(size(dev_involved,1)-size(dev_all_involved,1))+1:end) = [];
-        end
+        dev_all(:,s) = avg_dev;
         
-        dev_all(:,s) = dev;
-        dev_all_involved(:,s) = mean(dev_involved,2);
-
-        ch_devs_all(s) = biggest_dev;
+        % Get max dev for each ch
+        max_over_time = max(dev,[],1);
+        
+        % Find channel with biggest dev
+        [~,biggest_dev_ch] = max(max_over_time);
+        
+        ch_devs_all(s) = chs_involved(biggest_dev_ch);
         
     end
     
-    final_dev = nanmean(dev_all,2);
-    final_dev_involved = nanmean(dev_all_involved,2);
+    final_dev = mean(dev_all,2);
     
     % take mode channel with biggest dev
     ch_devs_all(ch_devs_all==0) = [];
     mode_ch = mode(ch_devs_all);
-
+    
+    mode_ch_dev = zeros(size(spike(1).data,1),nspikes);
+    
     soz_text = '';
     
     if pt_id ~= 0
@@ -149,26 +141,41 @@ for i = 1:length(listing)
                 end
                 
                 if ismember(mode_ch,soz) == 1
-                    soz_text = 'is soz';
+                    soz_text = '(is soz)';
                 else
-                    soz_text = 'is not soz';
+                    soz_text = '(is not soz)';
                 end
             end
         end
     end
     
-    
+    % get avg dev for that ch
+    for s = 1:length(spike)
+        x = spike(s).data;
+        x = pre_processing(x,do_car,pre_whiten,do_notch,fs);
+        x = x(:,mode_ch);
+        
+        bl = median(x,1);
+        d = abs(x-bl);
+        if size(mode_ch_dev,1) > size(d,1)
+            d = [d;...
+                repmat(d(end),size(mode_ch_dev,1)-size(d,1),1)];
+        elseif size(d,1) > size(mode_ch_dev,1)
+            d(end - (size(d,1)-size(mode_ch_dev,1))+1:end) = [];
+        end
+        mode_ch_dev(:,s) = d;
+    end
+    mode_ch_dev = mean(mode_ch_dev,2);
     
     figure
     set(gcf,'position',[440 58 932 740])
     subplot(2,1,1)
     plot(linspace(-surround,surround,length(final_dev)),final_dev)
-    title(sprintf('%s avg spike power\nin most involved ch\nmode channel %s',pt_name,soz_text))
+    title(sprintf('%s',pt_name))
     set(gca,'fontsize',20)
-    
     subplot(2,1,2)
-    plot(linspace(-surround,surround,length(final_dev)),final_dev_involved)
-    title(sprintf('%s avg spike power\nin all involved chs',pt_name))
+    plot(linspace(-surround,surround,length(final_dev)),mode_ch_dev)
+    title(sprintf('Biggest dev ch %s: %d',soz_text,mode_ch));
     set(gca,'fontsize',20)
     print([out_folder,pt_name],gcf,'-depsc');
     close(gcf)
