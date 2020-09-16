da90ebd9f5daa88f1eef19166856a6ff89b10628
Erin <erincconrad@gmail.com>
2020-09-16 11:35:40 -0400

    fixed bug in manual permanova script to restrict time window

--- compare_networks/manual_permanova.m
+++ compare_networks/manual_permanova.m
@@ -83,8 +83,9 @@ for j = 1:length(listing)
         early_idx = meta.spike(1).index_windows(:,1)/meta.fs < 2;
     end
     for s = 1:length(meta.spike)
+        meta.spike(s).index_windows(early_idx,:) = [];
         for f = 1:length(meta.spike(s).adj)
-            meta.spike(s).index_windows(early_idx,:) = [];
+            
             meta.spike(s).adj(f).adj(early_idx,:,:) = [];
         end
     end
