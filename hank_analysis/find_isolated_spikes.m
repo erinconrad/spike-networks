65af52930e00308577af279aec9afc257d78566b
Erin <erincconrad@gmail.com>
2021-04-30 15:41:54 -0400

    added code to look at hank data

--- hank_analysis/find_isolated_spikes.m
+++ hank_analysis/find_isolated_spikes.m
@@ -1,9 +1,16 @@
 fs = 2000;
+clip_times = [-2 0.4];
 
 %% Get file locations, load spike times and pt structure
 locations = spike_network_files;
 main_folder = locations.main_folder;
 hank_data_folder = locations.hank;
+data_folder = [main_folder,'data/'];
+out_folder = [data_folder,'hank/'];
+
+if exist(out_folder,'dir') == 0
+    mkdir(out_folder);
+end
 
 %% Load hank data
 ev = load([hank_data_folder,'temporalGeneral.mat']);
@@ -31,24 +38,31 @@ for p = 1:npts
     % Data
     values = dat.allExpThElecData{p}';
     
+    spike_clips = zeros(length(spikes),diff(round(clip_times*fs))+1);
+    
     % Loop through spikes
     for s = 1:length(spikes)
        
         idx = spikes(s);
         
         % Get data clip from 2s before to 0.4 seconds after
-        clip_times = [-2 0.4];
+        
         clip_indices = round(clip_times*fs)+idx;
         clip = values(clip_indices(1):clip_indices(2));
         
         % plot the clip
-        if 1
+        if 0
             figure
             plot(clip)
             pause
             hold off
         end
         
+        spike_clips(s,:) = clip;
+        
     end
+    
+    % Save
+    save([out_folder,sprintf('cat_%d.mat',p)],'spike_clips')
       
 end
